4cc5f58dee9065880f89f4fa5e33413817da83c4
J. Nathan Matias <natematias@gmail.com>
2016-06-06 23:40:20 -0400

    updated corpus processor to output on a running basis, to reduce memory usage

--- corpus-processing/create_subreddit_specific_files.py
+++ corpus-processing/create_subreddit_specific_files.py
@@ -4,21 +4,15 @@ import math
 import heapq
 import sys
 import csv
+import glob
 
 from pybloom import BloomFilter
 
-#TOP SUBS BY COMMENTS
-top_subs_by_comments = []
-with open('/mas/u/jnmatias/projects/reddit/reddit-scraper/data/top_75k_subreddits_by_comments.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        top_subs_by_comments.append(row)
 
-# CREATE BLOOMFILTER OF SUBS WITH MORE THAN 30 COMMENTS IN JUNE 2015
-tsbc = [x for x in top_subs_by_comments if int(x['comments'])>=30]
-sub_sample = BloomFilter(capacity=len(tsbc), error_rate=0.0001)
-[sub_sample.add(x['subname']) for x in tsbc]
-print "subreddits >30 comments a month: " + str(len(sub_sample))
+reddit_archive_dir = sys.argv[1]
+sub_archive_dir = sys.argv[2]
+print 
+print "outputting to " + sub_archive_dir
 
 # CREATE BLOOMFILTER OF BOTS
 bot_names = ['[deleted]', 'AutoModerator', 'autowikibot', 'TweetsInCommentsBot',
@@ -32,31 +26,34 @@ print "bots: " + str(len(bots))
 ### CREATE A JSON FILE FOR EACH SUBREDDIT
 subreddit_comments = defaultdict(list)
 counter = 0 
-for filename in ['/mas/u/jnmatias/smbshare/reddit-archive/comments/RC_2015-07',
-                 '/mas/u/jnmatias/smbshare/reddit-archive/comments/RC_2015-08',
-                 '/mas/u/jnmatias/smbshare/reddit-archive/comments/RC_2015-09']:
+for filename in sorted(glob.glob(reddit_archive_dir + "*")):
+    print filename
     for line in open(filename, 'r'):
         counter +=1
         if counter % 100000 == 0:
             sys.stdout.write(".")#str(counter) + ", ")
             sys.stdout.flush()
         comment = json.loads(line)
-        author = comment['author']
+        #author = comment['author']
         subreddit = comment['subreddit']
         subreddit_id = comment['subreddit_id']
-        if author in bots:
-            continue
-        if subreddit in sub_sample:
-            subreddit_comments[subreddit_id].append(line)
-            if len(subreddit_comments[subreddit_id])>=100000:
-                fd = open("./subreddits/" + subreddit_id +".json", "a")
-                fd.write("".join(subreddit_comments[subreddit_id]))
-                fd.close()
-                sys.stdout.write("x")
-                subreddit_comments[subreddit_id] = []
+        #if author in bots:
+        #    continue
+        subreddit_comments[subreddit_id].append(line)
+        # if there are more than 50000 comments for a subreddit
+        # in the dict, then write them out to file
+        if(len(subreddit_comments[subreddit_id])>25000):
+          fd = open(sub_archive_dir + subreddit_id +".json", "a") 
+          fd.write("".join(subreddit_comments[subreddit_id]))
+          fd.close()
+          sys.stdout.write("W")
+          subreddit_comments[subreddit_id] = []
+ 
+
+print "Found a total of %(count)s subreddits" % len(subreddit_comments.keys())
 
 for subreddit_id, sub in subreddit_comments.iteritems():
-    fd = open("/mas/u/jnmatias/projects/arrow_reddit_survival/subreddits/" + subreddit_id +".json", "a") 
+    fd = open(sub_archive_dir + subreddit_id +".json", "a") 
     fd.write("".join(sub))
     fd.close()
     print "writing " + subreddit_id
